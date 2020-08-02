9b9c2809892f9ddd11a47da6c337cfb57547de7e
bernethe <bernethe@bernethe.local>
2020-08-02 11:55:38 -0600

    docs agregado

---
+++ docs/precache-manifest.b861df6f56128c35c8af5b82d282c455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf88718566a873ec4588d1e223a4e0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6cb5d61ecdb80454953",
+    "url": "/static/css/main.9014f3a5.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cb5d61ecdb80454953",
+    "url": "/static/js/main.6a2fc546.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
